d5525573c86d3fb9247e1898c22245e86ccb73d5
MrDrummer25 <mrdrummer.programming@hotmail.com>
2018-10-10 10:40:33 +0100

    Added for loop

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 let jimp = require('jimp')
 
-async function resizeImage() {
+async function resizeImage(type) {
   try {
     let imgData = await jimp.read('original.jpg')
-    imgData.resize(3000, jimp.AUTO, jimp.RESIZE_BEZIER)
-    imgData.write('RESIZE_BEZIER.png')
+    imgData.resize(3000, jimp.AUTO, jimp[type])
+    imgData.write(type + '.png')
   } catch(e) {
     console.error("Couldn't read/manipulate that image!", e)
   }
 }
 
-resizeImage()
+let methods = [
+  "RESIZE_NEAREST_NEIGHBOR",
+  "RESIZE_BILINEAR",
+  "RESIZE_BICUBIC",
+  "RESIZE_HERMITE",
+  "RESIZE_BEZIER"
+]
+for (let method of methods) {
+  resizeImage(method)
+}
